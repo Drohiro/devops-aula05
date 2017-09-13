a3240aec4e69f0d8ecc6d103feb76786708c753d
Drohiro <rodrigorfsfr3@gmail.com>
2017-09-12 21:55:36 -0300

    Update testes.py

--- src/testes.py
+++ src/testes.py
@@ -1,12 +1,12 @@
 Import jogovelha
-imp9ort sys
+import sys
 
-erro9Inicializar = False
-jog999o = jogovelha.inicializar()
+erroInicializar = False
+jogo = jogovelha.inicializar()
 
-i9f le9n(jogo) != 3:
+if len(jogo) != 3:
 	erroInicializar = True
-el9e:
+ele:
 	for linha in jogo:
 		if len(linha) != 3:
 			erroInicializar = True
@@ -14,7 +14,7 @@ el9e:
 			for elemento in linha:
 				if elemento != '.':
 					erroInicializar = True
-if err9oInicializar:
-	9sys.exit(1)
+if erroInicializar:
+	sys.exit(1)
 els9e:
-	9sys.exit(0)
+	sys.exit(0)
